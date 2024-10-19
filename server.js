0f2c4d1082f34f4f3f1da3dea0f15530124fec23
mayconbalves <mayconbalves1@gmail.com>
2024-10-19 12:32:33 -0300

    feat(*): created header

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const cors = require('cors')
+const jsonServer = require("json-server");
+const cors = require("cors");
 
-server.use(cors())
-server.use(middlewares)
-server.use(router)
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 3000
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-	console.log(`JSON Server is running on port ${port}`)
-})
+  console.log(`JSON Server is running on port ${port}`);
+});
