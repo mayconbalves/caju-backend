ef7b8dea78176d9303c67c2bce61b599581c517c
mayconbalves <mayconbalves1@gmail.com>
2024-10-19 12:40:10 -0300

    fix(*): removed access control

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const middlewares = jsonServer.defaults();
 server.use(cors());
 server.use(middlewares);
 server.use(router);
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
