d23e7444c4ca6f7bc025bd6e20a7c3982e553d24
mayconbalves <mayconbalves1@gmail.com>
2024-10-24 11:41:09 -0300

    refactor: removed unnescessary file

--- api/server.js
+++ api/server.js
@@ -1,24 +1,26 @@
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const router = jsonServer.router(db);
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares))
-server.use(jsonServer.rewriter({
-	'/api/*': '/$1',
-	'/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
 server.listen(3000, () => {
-	console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
-module.exports = server
+module.exports = server;
